109159cde7a200727f645088270ad6c950454f46
pratik5005 <155246168+pratik5005@users.noreply.github.com>
2025-06-09 17:08:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-  base: process.env.VITE_BASE_PATH || "/magicbricks-clone",
 
+export default defineConfig({
+  plugins: [tailwindcss()],
+  base: './',
 })
